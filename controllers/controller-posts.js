9adb138aa0d26183cdf6afb302fc2a4bb2dc29e2
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-22 16:04:29 +0200

    [UPD] Updated destroy function

--- controllers/controller-posts.js
+++ controllers/controller-posts.js
@@ -18,7 +18,7 @@ const show = (req, res) => {
         })
     }
 
-    res.send(post);
+    res.json(post);
 }
 
 const store = (req, res) => {
@@ -27,26 +27,56 @@ const store = (req, res) => {
 
 const update = (req, res) => {
     let id = parseInt(req.params.id);
-    id >= 0 && id <= posts.length ? // Se il valore di req.params.id è compreso tra 0 e posts.length
-        res.send(`L'elemento ${id} è stato totalmente modificato`)
-        : // Altrimenti
-        res.send(`Il valore inserito non è valido`) // Restituisco un messaggio di errore
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    res.send(`L'elemento è stato totalmente modificato`);
 }
 
 const modify = (req, res) => {
     let id = parseInt(req.params.id);
-    id >= 0 && id <= posts.length ? // Se il valore di req.params.id è compreso tra 0 e posts.length
-        res.send(`L'elemento ${id} è stato parzialmente modificato`)
-        : // Altrimenti
-        res.send(`Il valore inserito non è valido`) // Restituisco un messaggio di errore
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    res.send(`L'elemento è stato parzialmente modificato`);
 }
 
 const destroy = (req, res) => {
     let id = parseInt(req.params.id);
-    id >= 0 && id <= posts.length ? // Se il valore di req.params.id è compreso tra 0 e posts.length
-        res.send(`L'elemento ${id} è stato eliminato`)
-        : // Altrimenti
-        res.send(`Il valore inserito non è valido`) // Restituisco un messaggio di errore
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    posts.splice(posts.indexOf(post), 1);
+
+    console.log(posts);
+    res.status(204);
 }
 
 module.exports = { index, show, store, update, modify, destroy };
