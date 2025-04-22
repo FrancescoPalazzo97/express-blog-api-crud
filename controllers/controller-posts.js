1c6c993801eb2a48518a9574032ed5ed57378d85
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-22 15:01:07 +0200

    [UPD] Added controller

---
+++ controllers/controller-posts.js
@@ -0,0 +1,23 @@
+const index = (req, res) => {
+
+}
+
+const show = (req, res) => {
+
+}
+
+const store = (req, res) => {
+
+}
+
+const update = (req, res) => {
+
+}
+
+const modify = (req, res) => {
+
+}
+
+const destroy = (req, res) => {
+
+}
