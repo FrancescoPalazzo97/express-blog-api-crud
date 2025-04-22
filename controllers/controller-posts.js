1fc76fe0d76b8414b58a2185c0a445ca0563b2d5
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-22 15:26:06 +0200

    [UPD] Controller imported

--- controllers/controller-posts.js
+++ controllers/controller-posts.js
@@ -1,23 +1,25 @@
 const index = (req, res) => {
-
+    res.send(`Elenco completo`)
 }
 
 const show = (req, res) => {
-
+    res.send(`Elemento singolo`)
 }
 
 const store = (req, res) => {
-
+    res.send(`Nuovo elemento creato`);
 }
 
 const update = (req, res) => {
-
+    res.send(`Elemento completamente modificato`);
 }
 
 const modify = (req, res) => {
-
+    res.send(`Elemento parzialmente modificato`);
 }
 
 const destroy = (req, res) => {
-
+    res.send(`Elemento eliminato`);
 }
+
+module.exports = { index, show, store, update, modify, destroy };
